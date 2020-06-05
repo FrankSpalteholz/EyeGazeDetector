256254233940376d2b1476679991819e84cc9620
ndeedy@nWksWin <ndeedy@me.com>
2020-06-05 14:55:52 +0200

    Add some comments

--- src/data_io.py
+++ src/data_io.py
@@ -1,6 +1,6 @@
 import os
 
-
+# Protected hardcoded root
 _OS_ROOT = {
     'nt': 'D:/Dropbox/work/Aikia/EyeTracker',
     'posix': '/Users/frankfurt/Dropbox/work/Aikia/EyeTracker'
@@ -8,13 +8,34 @@ _OS_ROOT = {
 
 
 def get_paths(sub_folder):
+    """
+    Getter for various project related paths
+
+    Args:
+        sub_folder (str): Sub-folder path
+
+    Returns:
+        tuple: 0 - input path
+               1 - output path
+               2 - configuration path
+               3 - data out path
+
+    """
+
+    # getting the root
     root = _OS_ROOT.get(os.name)
+
     if not root:
+        # raise exception if root is not found.
         raise RuntimeError('Current OS "{}" is not supported.'.format(root))
 
+    # If the host OS is Windows, replace path-separators
     if os.name == 'nt':
         root = root.replace('/', '\\')
 
+    # Build the paths. os.path.join always uses the correct os-separator.
+    # Appending correct os-separator to the end of string.
+    # For whatever reason :D
     io_path = os.path.join(root, 'footage', 'render', sub_folder) + os.sep
     conf_path = os.path.join(root, 'config') + os.sep
     data_out_path = os.path.join(root, sub_folder) + os.sep
